0998b719287f92b0cb1ef6f75f41aeb4db494298
Amanda Steidl <amanda.steidl@gmail.com>
2017-05-05 08:53:24 -0400

    Changing the isCozmo functionality

--- main.py
+++ main.py
@@ -33,78 +33,76 @@ def cozmo_program(robot: cozmo.robot.Robot):
         distance = (info[1] + con.COZMO_FRONT)
         veering = info[2]
 
-        if cozmoInFront == d.COZMO_AHEAD:
-
-
-        if d.TURN_RIGHT == decision_maker:
-            # Turn right, this is the wall case
-            driver.roadTurn(decision_maker, distance)
-
-        elif d.TURN_LEFT == decision_maker:
-            # Turn left, this is the wall case
-            driver.roadTurn(decision_maker, distance)
-
-        elif d.TURN_OPTIONAL_LEFT == decision_maker:
-            # There is currently an option to turn left
-            if driver.getWantTurn() == 1:
+        if cozmoInFront :
+            if d.STOP_AHEAD == decision_maker:
+                # Cozmo is ahead of you possibly stop
+                if len(info) != 4:
+                    print("Critical Error :: missing value in decision stop ahead.")
+                driver.setStop(80)
+                distance_to_stop = info[3]# Cozmo ahead stop
+                while True:
+                    temp, temp_isCozmoAhead = driver.getInfo()
+                    if temp_isCozmoAhead:
+                        break
+                driver.cozmoDriveDistance(distance_to_stop - 80)
+                direct = driver.getWhichWay()
+                # Make a turn using the turn options!
+                if direct == d.TURN_LEFT:
+                    driver.roadTurn(direct, (2*road_width + mid_width))
+                elif direct == d.TURN_RIGHT:
+                    driver.roadTurn(direct, road_width)
+            else :
+                # Cozmo is ahead, possibly moving
+                driver.setStop(80)
+                temp, temp_isCozmoAhead = driver.getInfo()
+                while (temp_isCozmoAhead):
+                    temp, temp_isCozmoAhead = driver.getInfo()
+                    continue
+        else:
+            if d.TURN_RIGHT == decision_maker:
+                # Turn right, this is the wall case
                 driver.roadTurn(decision_maker, distance)
-            else:
-                continue
 
-        elif d.TURN_OPTIONAL_RIGHT == decision_maker:
-            # There is currently an option to turn right
-            if driver.getWantTurn() == 1:
-                # 88.9 is the approximation of a lane
-                driver.roadTurn(decision_maker, (distance + road_width))
-            else:
-                continue
+            elif d.TURN_LEFT == decision_maker:
+                # Turn left, this is the wall case
+                driver.roadTurn(decision_maker, distance)
 
-        elif d.STOP_AHEAD == decision_maker:
-            # There is a stop sign ahead, decide what to do
-            driver.setStop(distance)
-            direct = driver.getWhichWay()
-            # Make a turn using the turn options!
-            if direct == d.TURN_LEFT:
-                driver.roadTurn(direct, (2*road_width + mid_width))
-            elif direct == d.TURN_RIGHT:
-                driver.roadTurn(direct, road_width)
+            elif d.TURN_OPTIONAL_LEFT == decision_maker:
+                # There is currently an option to turn left
+                if driver.getWantTurn() == 1:
+                    driver.roadTurn(decision_maker, distance)
+                else:
+                    continue
 
-        elif d.COZMO_AHEAD_STOP == decision_maker:
-            # Cozmo is ahead of you possibly stop
-            if len(info) != 4:
-                print("Critical Error :: missing value in decision stop ahead.")
-            driver.setStop(distance)
-            distance_to_stop = info[3] # Cozmo ahead stop
-            while True:
-                temp = driver.getInfo()
-                if temp[0] != d.COZMO_AHEAD_STOP:
-                    break
-            driver.cozmoDriveDistance(distance_to_stop)
-            direct = driver.getWhichWay()
-            # Make a turn using the turn options!
-            if direct == d.TURN_LEFT:
-                driver.roadTurn(direct, (2*road_width + mid_width))
-            elif direct == d.TURN_RIGHT:
-                driver.roadTurn(direct, road_width)
+            elif d.TURN_OPTIONAL_RIGHT == decision_maker:
+                # There is currently an option to turn right
+                if driver.getWantTurn() == 1:
+                    # 88.9 is the approximation of a lane
+                    driver.roadTurn(decision_maker, (distance + road_width))
+                else:
+                    continue
 
-        elif d.COZMO_AHEAD == decision_maker:
-            # Cozmo is ahead, possibly moving
-            driver.setStop(distance)
-        elif d.WAIT == decision_maker:
-            # Wait as in you're in traffic
-            driver.emergencyStop()
+            elif d.STOP_AHEAD == decision_maker:
+                # There is a stop sign ahead, decide what to do
+                driver.setStop(distance)
+                direct = driver.getWhichWay()
+                # Make a turn using the turn options!
+                if direct == d.TURN_LEFT:
+                    driver.roadTurn(direct, (2*road_width + mid_width))
+                elif direct == d.TURN_RIGHT:
+                    driver.roadTurn(direct, road_width)
 
-        elif d.SPEED_UPDATE == decision_maker:
-            # Update the speed to a new traffic pattern
-            if distance == -1:
-                continue
-            elif distance == 1:
-                driver.setNewLimit(True)
-            elif distance == 0:
-                driver.setNewLimit(False)
-            else:
-                print("Update Speed, incorrect distance value entered - ", distance)
-                print("should be 0, 1, -1 :: Error within d.SPEED_UPDATE in main.py")
+            elif d.SPEED_UPDATE == decision_maker:
+                # Update the speed to a new traffic pattern
+                if distance == -1:
+                    continue
+                elif distance == 1:
+                    driver.setNewLimit(True)
+                elif distance == 0:
+                    driver.setNewLimit(False)
+                else:
+                    print("Update Speed, incorrect distance value entered - ", distance)
+                    print("should be 0, 1, -1 :: Error within d.SPEED_UPDATE in main.py")
 
         # VEERING SHOULD BE HANDLED ALWAYS
         if d.CONTINUE == veering:
