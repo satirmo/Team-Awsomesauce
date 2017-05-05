d8bdec33a020932be04cd8dcb508f4d12345e9d7
John Atti <thegreatbunsenburner@gmail.com>
2017-05-05 00:32:20 -0400

    Cozmo logic update - woo!
    Updated CozmoDecisions class with more logic.

--- CozmoDecisions.py
+++ CozmoDecisions.py
@@ -31,16 +31,16 @@ class CozmoObstacleCheck:
         # stores all seen signs as a list (for comparison), from Tomas' code
         # Names: "triangle left" "triangle right" "square" "pentagon left"
         #        "pentagon right" "octagon" "circle" "cozmo"
-        self.allSignsList=[[(0,100),(1,100),(2,100),(3,100),(4,100),(5,100),(6,100),(7,100),(8,100)],(1,1)]
+        self.allSignsList=[[(0,100),(1,100),(2,100),(3,100),(4,100),(5,100),(6,100),(7,100),(8,100)],(110,91)]
 
         # stores current sign list without veering information
         self.currentSignList=[]
 
         # stored lane distance
-        self.laneDistances=(110,90)
+        self.laneDistances=(110,91)
 
         # stores directions for veering
-        self.veeringDirections=()
+        self.veeringDirections=11
 
         # stores the max distance that an object should be recognized by the Cozmo
         self.DISTANCE_THRESHOLD=60
@@ -111,10 +111,11 @@ class CozmoObstacleCheck:
         # ----need Tomas' functions to call the signs. Will take 5 pictures,
         # compare results, and determine what kind of signs are present.
         #allSignsList=[[ (sign_1, dist_1), (sign_2, dist_2), ..., (sign_n, dist_n) ],(leftDist, rightDist, isBehindCozmo)]
-        self.allSignsList=[[('triangle left',100),("triangle right",100),
-                            ('square',100),('pentagon left',50),
-                            ('pentagon right',100),('octagon',100),
-                            ('circle',20),('cozmo',100)],(1,1)]
+        for i in range(5):
+            self.allSignsList=[[('triangle left',100),('triangle right',100),
+                                ('square',100),('pentagon left',500),
+                                ('pentagon right',100),('octagon',1),
+                                ('circle',200),('cozmo',100)],(110,91)]
 
         # run pruneSigns to separate veering from sign design
         self.pruneSigns()
@@ -169,40 +170,62 @@ class CozmoObstacleCheck:
         # our thresholds for this right now, tentatively, are:
         # >=159 for being too far from one of the sides, and
         # <=75 for being too close to one of the sides
-        #print("testing veering")
+        # note: CORRECT_LEFT means to go faster on the left wheel
+        #       CORRECT_RIGHT means to faster on the right wheel
+        highThreshold=130
+        lowThreshold=90
 
         # if left and right are both between 90 and 140
         if(self.laneDistances[0] >= 85 and self.laneDistances[0] <= 135 and
             self.laneDistances[1] >= 85 and self.laneDistances[1] <= 135):
-
             # return directions for staying on course
-            self.veeringDirections=(x.CONTINUE)
-            pass
-        # if left is less than 90
+            self.veeringDirections=self.x.CONTINUE
+
+        # if left lane is close
+        elif(self.laneDistances[0] < lowThreshold):
             # adjust away from the left (make the number bigger)
+            self.veeringDirections=self.x.CORRECT_LEFT
 
-        # if left is more than 130
-            # check other lane to see if it's within its threshold. ----------
+        # if left lane is far
+        elif(self.laneDistances[0] >= highThreshold):
+            # check other lane to see if it's within its threshold.
+            # If it's too close, adjust.
+            if(self.laneDistances[1] < lowThreshold):
             # adjust towards the left (make the number smaller)
+                self.veeringDirections=self.x.CORRECT_RIGHT
+            else:
+                self.veeringDirections=self.x.CONTINUE
 
-        # if right is less than 90
+        # if right lane is close
+        elif(self.laneDistances[1] < lowThreshold):
             # adjust away from the right (make the number bigger)
+            self.veeringDirections=self.x.CORRECT_RIGHT
 
-        # if right is more than 130
-            # adjust towards the right (make the number smaller)
-
-        pass
+        # if right lane is far
+        elif(self.laneDistances[1] >= highThreshold):
+            # check other lane to see if it's within its threshold.
+            # If it's too close, adjust.
+            if(self.laneDistances[0] < lowThreshold):
+                self.veeringDirections=(self.x.CORRECT_LEFT)
+            # otherwise, continue as normal
+            else:
+                self.veeringDirections=(self.x.CONTINUE)
+        else:
+            self.veeringDirections=(self.x.CONTINUE)
+        return self.veeringDirections
 
     # this is the main function of the class
     # it calls all the other functions and
     # returns the directions for the Cozmo
     # previously titled "logicBomb"
     def returnDirections(self):
+        # ensure that the current directions list is cleared
+        self.directionList=[]
+
         # run interpretSigns to split Tomas' data into the foundation for
         # veering directions and sign response
-
-        # run interpretSigns
         self.interpretSigns()
+
         # run compareLast
         # note: we may not need to do this anymore due to advancement
         #       in openCV. Hazaa!
@@ -210,41 +233,62 @@ class CozmoObstacleCheck:
         # call storeLast to save old signs
         self.storeLast()
 
+        # check veering status
+        self.veeringDirections=self.checkVeering()
+
         # Begin analysis to determine next direction list for Cozmo. This is
         # essentially a large switch statement. Only Python doesn't do switch
         # statements, so we'll be using if/else
 
         # If only 1 sign is noted, return directions for that sign
         # based on comparison and distance
-        if(0):
+        if(self.signFocus==1):
+
             # if stop sign is seen first
             if(self.currentSignList[0][0]=='octagon'):
                 # if within our DISTANCE_THRESHOLD, initiate turn with distance needed
-                if(self.currentSignList[0][1]<DISTANCE_THRESHOLD):
-                    pass
+                if(self.currentSignList[0][1]<self.DISTANCE_THRESHOLD):
+                    self.directionList=[self.x.STOP_AHEAD,self.currentSignList[0][1],self.veeringDirections]
+                    return self.directionList
                 # if outside of our DISTANCE_THRESHOLD, continue as normal and rescan
                 else:
-                    self.directionList[0][0]=self.x.CONTINUE
-                pass
+                    self.directionList=[self.x.CONTINUE,-1,self.veeringDirections]
+                    return self.directionList
+
             # if another Cozmo is infront in same lane
+            elif(self.currentSignList[0][0]=='cozmo'):
                 # instruct Cozmo to move the distance between the two objects
-                # then stop and wait for the other object to move
+                # then stop and rescan until the other object moves
+                self.directionList=[self.x.CONTINUE,self.currentSignList[0][1],self.veeringDirections]
+                return self.directionList
 
             # if left turn sign
+            elif(self.currentSignList[0][0]=='pentagon left'):
                 # if within our DISTANCE_THRESHOLD, initiate turn with distance needed
-
+                if(self.currentSignList[0][1]<self.DISTANCE_THRESHOLD):
+                    self.directionList=[self.x.TURN_LEFT,self.currentSignList[0][1],self.veeringDirections]
+                    return self.directionList
                 # if outside of DISTANCE_THRESHOLD, continue as normal until within range
+                else:
+                    self.directionList=[self.x.CONTINUE,-1,self.veeringDirections]
+                    return self.directionList
 
             # if right turn sign
+            elif(self.currentSignList[0][0]=='pentagon right'):
                 # if within our DISTANCE_THRESHOLD, initiate turn with distance needed
-
+                if(self.currentSignList[0][1]<self.DISTANCE_THRESHOLD):
+                    self.directionList=[self.x.TURN_RIGHT,self.currentSignList[0][1],self.veeringDirections]
+                    return self.directionList
                 # if outside of DISTANCE_THRESHOLD, continue as normal until within range
-
+                else:
+                    self.directionList=[self.x.CONTINUE,-1,self.veeringDirections]
+                    return self.directionList
             # if optional left turn sign
             if(0):
                 # draw a random number within an if statement to determine turning
                 if(random.randrange(0,2)):
                     #turn left
+                    
                     pass
                 else:
                     # stay straight
@@ -283,8 +327,7 @@ class CozmoObstacleCheck:
                     # if the Cozmo is close enough to act on the stop sign,
                     # return call to stop
 
-        # check veering
-        self.checkVeering()
+
 
         return self.directionList
 
@@ -297,12 +340,13 @@ class CozmoObstacleCheck:
         # print(self.currentSignList)
         # print(self.veeringDirections)
         print(self.directionList)
-        return
+        return self.directionList
 
 
 # temp main used to test the class. Makes sure the directions transfer over.
 one=CozmoObstacleCheck()
-one.testThis()
+ourList=one.testThis()
+print(ourList)
 #x=constants.decisions()
 
 
