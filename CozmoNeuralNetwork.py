9b1fc60d292aef23e58ee695ebb20069eb107fe3
Tomas Carino-Bazan <satirmo@gmail.com>
2017-05-07 14:19:52 -0400

    Update CozmoNeuralNetwork.py

--- CozmoNeuralNetwork.py
+++ CozmoNeuralNetwork.py
@@ -8,86 +8,57 @@ import imutils
 import cv2
 import os
 
-class CozmoNeuralNet :
+# Cozmo Neural Network Class for identifying situations in which
+# other Cozmos are blocking its path
+class CozmoNeuralNetwork :
 	def __init__( self ) :
-		print("TOMAS IS ALIVE")
 		rawImages = []
 		features = []
 		labels = []
 
-		for i in range( 1, 1001 ) : #1001
-			number = str( i );
-			if i <= 500 : #500
-				label = True;
-
-			else :
-				label = False;
-
-			# Update path as needed
-			path = "C:\\Users\Amandas\Desktop\OpenCV\Examples\cozmo-python-sdk\cozmoDrives\Team-Awsomesauce\Training_Data\\train" + number + ".png";
+		for i in range( 1, 1001 ) :
+			# Get image
+			path = "path/to/file/" + str( i ) + ".png";
 			image = cv2.imread( path );
 
-			pixels = self.image_to_feature_vector(image)
-			hist = self.extract_color_histogram(image)
+			# Get label
+			label = True if i <= 500 else False;
 
-			# update the raw images, features, and labels matricies,
-			# respectively
-			rawImages.append(pixels)
-			features.append(hist)
-			labels.append(label)
+			# Process Image
+			pixels = self.image_to_feature_vector( image );
+			hist = self.extract_color_histogram( image );
 
-		# show some information on the memory consumed by the raw images
-		# matrix and features matrix
-		rawImages = np.array(rawImages)
-		features = np.array(features)
-		labels = np.array(labels)
+			# Update rawImages, features, and labels
+			rawImages.append( pixels );
+			features.append( hist );
+			labels.append( label );
 
-		# partition the data into training and testing splits, using 75%
-		# of the data for training and the remaining 25% for testing
-		(trainRI, testRI, trainRL, testRL) = train_test_split(
-			rawImages, labels, test_size=0.25, random_state=42)
-		(trainFeat, testFeat, trainLabels, testLabels) = train_test_split(
-			features, labels, test_size=0.25, random_state=42)
+		# Convert rawImages, features, and labels to numpy array
+		rawImages = np.array( rawImages );
+		features = np.array( features );
+		labels = np.array( labels );
 
-		# train and evaluate a k-NN classifer on the histogram
-		# representations
-		print("[INFO] evaluating histogram accuracy...")
-		self.model = KNeighborsClassifier(n_neighbors=10,
-			n_jobs=-1)
-		self.model.fit(trainFeat, trainLabels)
-		acc = self.model.score(testFeat, testLabels)
-		print("[INFO] histogram accuracy: {:.2f}%".format(acc * 100))
+		# Partition data into training and testing data
+		( trainRI, testRI, trainRL, testRL ) = train_test_split( rawImages, labels, test_size = 0.25, random_state = 42 );
+		( trainFeat, testFeat, trainLabels, testLabels ) = train_test_split( features, labels, test_size = 0.25, random_state = 42 );
 
-	def image_to_feature_vector(self, image, size=(32, 32)):
-		return cv2.resize(image, size).flatten()
+		# Train neural network
+		self.model = KNeighborsClassifier( n_neighbors = 5, n_jobs = -1 );
+		self.model.fit( trainFeat, trainLabels );
 
-	def extract_color_histogram(self, image, bins=(8, 8, 8)):
-		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-		hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,
-			[0, 180, 0, 256, 0, 256])
+	# Convert image to feature vector
+	def image_to_feature_vector( self, image, size = ( 32, 32 ) ) :
+		return cv2.resize( image, size ).flatten();
+
+	# Get color histogram of image
+	def extract_color_histogram( self, image, bins = ( 8, 8, 8 ) ) :
+		hsv = cv2.cvtColor( image, cv2.COLOR_BGR2HSV );
+		hist = cv2.calcHist( [ hsv ], [ 0, 1, 2 ], None, bins, [ 0, 180, 0, 256, 0, 256 ] );
 
 		if imutils.is_cv2():
-			hist = cv2.normalize(hist)
+			hist = cv2.normalize( hist );
 
 		else:
-			cv2.normalize(hist, hist)
+			cv2.normalize( hist, hist );
 
 		return hist.flatten()
-
-
-
-
-# if __name__ == "__main__" :
-# 	cnn = CozmoNeuralNet();
-#
-# 	for i in range( 1, 30 ) :
-# 		path = "C:\\Users\Amandas\Desktop\OpenCV\Examples\cozmo-python-sdk\cozmoDrives\Team-Awsomesauce\Test Data\\test" + str( i ) + ".png";
-# 		image = cv2.imread( path );
-#
-# 		hist = cnn.extract_color_histogram(image)
-# 		result = cnn.model.predict( [hist] )[ 0 ]
-#
-# 		print( "result", i, ":", result );
-#
-# 		cv2.imshow( "sample", image );
-# 		cv2.waitKey();
